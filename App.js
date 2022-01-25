45fec7b978a11533467380f7f62233e61ee06d03
Blnkfc <pavlinus11v@gmail.com>
2022-01-25 21:36:44 +0200

    Added News folder to the project, routing to be soon.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
+import News from "./components/News/News"
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 const App = (props) => {
@@ -14,6 +15,7 @@ const App = (props) => {
                 <Switch>
                     <Route path="/profile" > <Profile profile={props.profile} /> </Route>
                     <Route path="/dialogs" > <Dialogs dialogs={props.dialogs}/> </Route>
+                    <Route path="/news" > <News dialogs={props.dialogs}/> </Route>
                 </Switch>
             </div>
         </Router>
---
+++ components/News/News.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./News.css"
+
+const News = () =>{
+    return<div className={"news"}>
+
+    </div>
+}
+
+export default News;
